a6b1b26df161d2c8f5a47897bc8317b90823b2ee
Mohammad Shaaban <87359096+MrSha3ban@users.noreply.github.com>
2026-02-22 13:34:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+const CACHE_NAME = 'markercad-v1';
+
+// We just need it to install and stay active
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
 self.addEventListener('fetch', (event) => {
+  // Essential for PWA offline support
   event.respondWith(fetch(event.request));
 });
